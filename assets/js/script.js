44f92e6af16969cc4fd7a27ca6cd7d5bebe0d030
Tom Hsu <thomaschsu@gmail.com>
2018-05-21 00:47:54 -0400

    Modified js animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 $(function(){
     $("#main-container").hide().fadeIn(2000);
     $("#alert-msg").hide().fadeIn(2000);
+    var navbar = $(".navbar-brand");
+    navbar.animate({fontSize: '35px'}, "slow");
  });
