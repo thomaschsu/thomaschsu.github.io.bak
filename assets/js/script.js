6ba95a5e8c650db7224d936af90b9b5b5248cd11
Tom Hsu <thomaschsu@gmail.com>
2018-10-24 23:19:58 -0400

    Removed mouse icon and added back to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,23 @@
-// Drop down for navbar
-$(".dropdown-trigger").dropdown();
-
-// Smooth scroll
-$("a[href^='#']").click(function(e) {
-  e.preventDefault();
-
-  var position = $($(this).attr("href")).offset().top;
-
-  $("body, html").animate({
-      scrollTop: position
-  } /* speed */ );
-});
-
-$(document).ready(function(){
+$(document).ready(function () {
   $('.collapsible').collapsible();
+  $('.modal').modal();
 });
 
-$(document).ready(function(){
-  $('.modal').modal();
+$(document).ready(function () {
+
+  //Check to see if the window is top if not then display button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollUp').fadeIn();
+    } else {
+      $('.scrollUp').fadeOut();
+    }
+  });
+
+  //Click event to scroll to top
+  $('.scrollUp').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 800);
+    return false;
+  });
+
 });
